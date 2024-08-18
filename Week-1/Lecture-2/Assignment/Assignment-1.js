80c2ce867295f0de7ccbdbcd11db0883e4c67136
nayan <nayan_s@ce.iitr.ac.in>
2024-08-18 15:09:27 +0530

    1.0

---
+++ Week-1/Lecture-2/Assignment/Assignment-1.js
@@ -0,0 +1,5 @@
+//Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+let  favorite_Color="black";
+const height=180;
+const like_pizza="true";
+console.log(`My favorite color is ${favorite_Color}, my height is ${height} cm and I like pizza ${like_pizza}.`);
